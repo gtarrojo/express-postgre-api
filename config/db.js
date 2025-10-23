14024a0f6a532ce6ff1df2b6e822a1b4d40a8009
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-10-23 13:46:06 +0200

    create postgres pool connection

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DBPORT,
+});
+
+pool.on("connect", () => {
+  console.log("connection pool established with Database");
+});
+
+export default pool;
